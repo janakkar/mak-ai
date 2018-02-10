3d6645743e02ac6019911b966af6776a19e9adcc
Jana Karcheska <jana.karcheska@netcetera.com>
2018-02-10 19:25:10 +0100

    log all questions and answers

--- mak.js
+++ mak.js
@@ -9,28 +9,31 @@ const rl = readline.createInterface({
 
 const test = [{
     "ask": "Anything I need to know about?",
-    "id": 1001
+    "yes": {
+        "ask": "What is it?"
+    }
 }, {
-    "ask": "Anything else?",
-    "id": 1002
+    "ask": "Anything else?"
 },
 {
     "ask": "You sure?",
     "id": 1003
 }];
 
-const askQuestion = (question) => new Task((rej, res) => rl.question(question, res));
-const logAnswer = (answer) => {
-    console.log(`Your answer is ${answer}`);
+const askQuestion = (question) => new Task((rej, res) =>
+    rl.question(question, (answer) => res({ question, answer })));
+
+const logAnswer = ({ question, answer }) => {
+    console.log(`Your answer for question: ${question}, is: ${answer}`);
 };
 
-const logAnswerAskNextQuestion = (answer, question) => {
-    logAnswer(answer);
+const logAnswerAskNextQuestion = (answered, question) => {
+    console.log('----@'); logAnswer(answered);
     return askQuestion(question);
 };
 
 const askAllQuestions = _.reduce((t, q) => {
-    return t ? t.chain(answer => logAnswerAskNextQuestion(answer, q)) : askQuestion(q);
+    return t ? t.chain(answered => logAnswerAskNextQuestion(answered, q)) : askQuestion(q);
 }, undefined);
 
 const getAllQuestions = _.map(_.prop('ask'));
