5a15bbb0095d1fd1acd0e9e4cc0a09048a5e0417
Jana Karcheska <jana.karcheska@netcetera.com>
2018-02-10 19:35:48 +0100

    deeper nested tree of questions

--- mak.js
+++ mak.js
@@ -10,7 +10,10 @@ const rl = readline.createInterface({
 const test = [{
     "ask": "Anything I need to know about?",
     "yes": {
-        "ask": "What is it?"
+        "ask": "What is it?",
+        "hm": {
+            "ask": "Think harder..."
+        }
     }
 }, {
     "ask": "Anything else?"
@@ -20,32 +23,41 @@ const test = [{
     "id": 1003
 }];
 
-const askQuestion = (question) => new Task((rej, res) =>
-    rl.question(question.ask, (answer) => res({question, answer})));
+const askQuestion = (question, answers) => new Task((rej, res) =>
+    rl.question(question.ask, (answer) => res({question, answer, answers})));
 
 const logAnswer = ({question, answer}) => {
     console.log(`Your answer for question: ${question.ask}, is: ${answer}`);
 };
 
-const logAnswerAskNextQuestion = ({question, answer}, nextQuestion) => {
+const collectAnswers = ({question, answer, answers}) => {
     logAnswer({question, answer});
+    return {question, answer,
+        'answers': _.append({question, answer}, answers)};
+}
+
+const collectAnswerAskNextQuestion = ({question, answer, answers}, nextQuestion) => {
+    const collectedAnswers = collectAnswers({question, answer, answers}).answers;
     const followUpQuestion = question[answer];
     return followUpQuestion ?
-        askQuestion(followUpQuestion).chain(answered => logAnswerAskNextQuestion(answered, nextQuestion))
-        : askQuestion(nextQuestion);
+        askQuestion(followUpQuestion, collectedAnswers).chain(answered =>
+            collectAnswerAskNextQuestion(answered, nextQuestion))
+        : askQuestion(nextQuestion, collectedAnswers);
 };
 
 const askAllQuestions = _.reduce((task, nextQuestion) => {
     return task ? task.chain(answered =>
-        logAnswerAskNextQuestion(answered, nextQuestion)) : askQuestion(nextQuestion);
+        collectAnswerAskNextQuestion(answered, nextQuestion)) : askQuestion(nextQuestion, []);
 }, undefined);
 
-const getAllQuestions = _.map(_.identity);
 
-const mak = (listOfQuestions) => askAllQuestions(getAllQuestions(listOfQuestions));
+const mak = askAllQuestions;
 
-mak(test).fork(console.error, logAnswer);
-
-
-
-
+mak(test).fork(console.error, (answered) => {
+    const collected = collectAnswers(answered);
+    
+    console.log('-----------------------------------');
+    console.log('YOUR ANSWERS');
+    console.log('-----------------------------------');
+    collected.answers.map(logAnswer);
+});
