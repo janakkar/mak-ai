a115d96f7f6a16ed81d70b2562c802a2c0e57bc7
Jana Karcheska <jana.karcheska@netcetera.com>
2018-02-17 20:39:02 +0100

    refactor with generators

--- mak.js
+++ mak.js
@@ -6,44 +6,44 @@ const rl = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
-
-const askQuestion = (question, context = { answers: [] }) => new Task((rej, res) =>
-    rl.question(question.ask, (answer) => res({ question, answer, context })));
-
 const logAnswer = ({ question, answer }) => {
     console.log(`Your answer for question: ${question.ask}, is: ${answer}`);
 };
 
-const collectAnswers = (answered) => {
-    const { question, answer } = answered;
-    answered.context.answers.push({ question, answer });
-    return answered;
-}
+const questionToTask = (context) => new Task((rej, res) =>
+    rl.question(context.question.ask, (answer) => {
+        context.answer = answer;
+        context.answers = _.append({ question: context.question, answer }, context.answers);
+        res(context);
+    }));
 
-const askFollowUpQuestions = (answered, nextQuestion) => {
-    const { question, answer } = answered;
-    const updatedContext = collectAnswers(answered).context;
-    const followUpQuestions = question[answer];
-    if (followUpQuestions) {
-        let followUpQuestionsTask =
-            _.reduce((task, followUpQuestion) =>
-                task ? task.chain(answered => askFollowUpQuestions(answered, followUpQuestion))
-                    : askQuestion(followUpQuestion, updatedContext), undefined)(followUpQuestions);
-        followUpQuestionsTask = followUpQuestionsTask
-            .chain(answered => askFollowUpQuestions(answered, nextQuestion));
-        return followUpQuestionsTask;
-    } else if (nextQuestion) {
-        return askQuestion(nextQuestion, updatedContext);
-    }
-    return new Task((rej, res) => res(answered));
+const createQuestionsGenerator = (questions, context) =>
+    function* () {
+        for (i in questions) {
+            context.question = questions[i];
+            const followUpContext = yield questionToTask(context);
+            const followUpQuestions = getFollowUpQuestions(followUpContext);
+            if (followUpQuestions) {
+                console.log(followUpQuestions);
+                yield* createQuestionsGenerator(followUpQuestions, followUpContext);
+            }
+        }
+    }();
+
+const getFollowUpQuestions = (context) => {
+    return context.question[context.answer];
 };
 
-const askAllQuestions = (questions) => (_.reduce((task, nextQuestion) => {
-    return task ? task.chain(answered =>
-        askFollowUpQuestions(answered, nextQuestion)) : askQuestion(nextQuestion);
-}, undefined)(questions)).chain(answered => askFollowUpQuestions(answered, null));
+const mak = (questions, context = { answers: [] }) => {
+    const gen = createQuestionsGenerator(questions, context);
+    const step = value => {
+        const result = gen.next(value)
+        return result.done ? Task.of(value) : result.value.chain(step)
+    };
+    return step();
+}
 
-module.exports = { askAllQuestions, collectAnswers };
+//module.exports = { askAllQuestions, collectAnswers };
 
 const test = [{
     "ask": "Anything I need to know about?",
@@ -64,10 +64,11 @@ const test = [{
         "ask": "What are you waiting for?"
     }]
 }];
-const mak = askAllQuestions;
-mak(test).fork(console.error, (answered) => {
-    console.log('-----------------------------------');
-    console.log('YOUR ANSWERS');
-    console.log('-----------------------------------');
-    answered.context.answers.map(logAnswer);
-});
+mak(test)
+    .fork(console.error, (context) => {
+        console.log(context);
+        console.log('-----------------------------------');
+        console.log('YOUR ANSWERS');
+        console.log('-----------------------------------');
+        context.answers.map(logAnswer);
+    });
